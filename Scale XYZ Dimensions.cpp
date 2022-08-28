44e3860fbb23bf5b7bca0705afcbaf73f71a49a6
Piotr Krol <krolpiotr225@gmail.com>
2022-08-28 21:58:58 +0200

    Create command

--- Scale XYZ Dimensions.cpp
+++ Scale XYZ Dimensions.cpp
@@ -1,7 +1,10 @@
-
 #include <Core/CoreAll.h>
 #include <Fusion/FusionAll.h>
 #include <CAM/CAMAll.h>
+#include <iostream>
+#include <format>
+#include <string>
+#include <Fusion/BRep/BRepBody.h>
 
 
 using namespace adsk::core;
@@ -11,8 +14,41 @@ using namespace adsk::cam;
 Ptr<Application> app;
 Ptr<UserInterface> ui;
 
+
+// CommandCreated event handler.
+class ScaleCommandCreatedEventHandler : public adsk::core::CommandCreatedEventHandler
+{
+public:
+	void notify(const Ptr<CommandCreatedEventArgs>& eventArgs) override
+	{
+		app->log("crev");
+	}
+
+private:
+	
+}_cmdCreated;
+
+
+bool checkReturn(Ptr<Base> returnObj)
+{
+	if (returnObj)
+		return true;
+	else
+		if (app && ui)
+		{
+			std::string errDesc;
+			app->getLastError(&errDesc);
+			ui->messageBox(errDesc);
+			return false;
+		}
+		else
+			return false;
+
+	}
+
 extern "C" XI_EXPORT bool run(const char* context)
 {
+	
 	app = Application::get();
 	if (!app)
 		return false;
@@ -21,18 +57,42 @@ extern "C" XI_EXPORT bool run(const char* context)
 	if (!ui)
 		return false;
 
-	ui->messageBox("Hello addin");
+
+	// Create a command definition and add a button to the CREATE panel.
+	Ptr<CommandDefinition> cmdDef = ui->commandDefinitions()->addButtonDefinition("ScaleXYZCPPAddIn", "Scale XYZ Dimensions", "a Mill relief component", "Resources");
+	if (!checkReturn(cmdDef))
+		return false;
+
+	Ptr<ToolbarPanel> createPanel = ui->allToolbarPanels()->itemById("SolidModifyPanel");
+	if (!checkReturn(createPanel))
+		return false;
+
+	Ptr<CommandControl> scaleXYZButton = createPanel->controls()->addCommand(cmdDef);
+	if (!checkReturn(scaleXYZButton))
+		return false;
+
+	// Connect to the Command Created event.
+	Ptr<CommandCreatedEvent> commandCreatedEvent = cmdDef->commandCreated();
+	commandCreatedEvent->add(&_cmdCreated);
+	if (!checkReturn(commandCreatedEvent))
+		return false;
 
 	return true;
 }
 
 extern "C" XI_EXPORT bool stop(const char* context)
 {
-	if (ui)
-	{
-		ui->messageBox("Stop addin");
-		ui = nullptr;
-	}
+	Ptr<ToolbarPanel> createPanel = ui->allToolbarPanels()->itemById("SolidModifyPanel");
+	if (!checkReturn(createPanel))
+		return false;
+
+	Ptr<CommandControl> scaleXYZ = createPanel->controls()->itemById("ScaleXYZCPPAddIn");
+	if (checkReturn(scaleXYZ))
+		scaleXYZ->deleteMe();
+
+	Ptr<CommandDefinition> cmdDef = ui->commandDefinitions()->itemById("ScaleXYZCPPAddIn");
+	if (checkReturn(cmdDef))
+		cmdDef->deleteMe();
 
 	return true;
 }
